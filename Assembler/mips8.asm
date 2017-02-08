2100d54356a5da7a9dc06b162c71e2647f4a7e38
RamanRaman <raman.larionov@yandex.ru>
2017-02-08 18:32:50 +0300

    Revert "educ"
    This reverts commit 100b034fb03f6a22b142131cc5fcc618cf8a651f.

--- Assembler/mips8.asm
+++
@@ -1,6 +0,0 @@
-.text
-.globl function
-function:
-         move $v0 $a0 
-         move $v1 $a1 
-         jr   $ra
--- Assembler/Щеп/Pz-dz/PZ -11/done.asm
+++
@@ -1,4 +0,0 @@
-.macro done    #Макроопределение завершения программы.
-	li $v0,10
-	syscall
-.end_macro
